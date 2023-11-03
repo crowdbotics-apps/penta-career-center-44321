b8dd995c01032428729af9d685c6e14d76de2a3e
Crowdbotics <team@crowdbotics.com>
2023-11-03 18:22:47 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "penta-career-center-44321",
+  url: "https://penta-career-center-44321.botics.co",
+  api: "https://penta-career-center-44321.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
